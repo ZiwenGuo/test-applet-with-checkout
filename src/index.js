720dab5227f2af712621081e75571c4bf4227d0c
Ziwen Guo <guoziwenapp@gmail.com>
2021-04-13 11:05:13 -0700

    add FBInstant sdk

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+(function () {
+  var firstScript = document.getElementsByTagName("script")[0]
+  if (document.getElementById("applet-js-sdk")) {
+    return
+  }
+  var sdk = document.createElement("script")
+  sdk.id = "applet-js-sdk"
+  //sdk.src = "https://connect.facebook.net/en_US/fbapplet.latest.js";
+  sdk.src = 'https://www.facebook.com/assets.php/en_US/fbinstant.latest.js'
+  firstScript.parentNode.insertBefore(sdk, firstScript)
+  console.log('sdk inserted')
+})();
+
+console.log(window)
+console.log(window.FBInstant)
+
+window.FBInstant.initializeAsync()
+  .then(() => {
+    return window.FBInstant.startGameAsync();
+  })
+  .then(startApp)
+  .catch((err) => {
+    console.log(err);
+  });
+
+function startApp() {
+  console.log('start app')
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
